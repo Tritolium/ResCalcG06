39aef7f75cc570a37e8b178fafe0ed079f7cbc81
Tritolium <paracrafta@gmail.com>
2018-04-19 13:12:11 +0200

    Initial commit

---
+++ app/src/main/java/v1/hapra/rescalcg06/ResCalcG06Activity.java
@@ -0,0 +1,13 @@
+package v1.hapra.rescalcg06;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResCalcG06Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res_calc_g06);
+    }
+}
